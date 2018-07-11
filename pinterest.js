bd3df0287708bc1cca4ff3243bb0385eac359ba6
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-11 16:46:53 -0700

    New functions

--- pinterest.js
+++ pinterest.js
@@ -1,40 +1,70 @@
-var Const = require('./const');
-
 //  Initialize once with app id
-PDK.init({ appId: Const.PIN_APP, cookie: true });
+PDK.init({ appId: '4976224077197881480', cookie: true });
 
-//  Pinterest SDK actions
-var Pinterest = {
-    /*
-     *  Use the SDK to log into the user's Pinterest
-     *  param [Function] callback - function started when complete
-     */
-    login: function(callback) {
-        PDK.login({ scope : Const.PIN_SCOPE }, callback);
-    },
-    //  Use the SDK to log out of  the user's Pinterest
-    logout: function() {
-        PDK.logout();
-    },
-    //  Use SDK to determine auth state of user
-    loggedIn: function() {
-        return !!PDK.getSession();
-    },
-    /*
-     *  Use SDK to delete a duplicate Pin
-     *  param [Object]   data     - {board, note, link, image_url}
-     *  param [Function] callback - function started when complete
-     */
-    deletePin: function(data, callback) {
-        PDK.request('/pins/', 'DELETE', data, callback);
-    },
-    /*
-     *  Use SDK to request user's boards
-     *  param {Function} callback - function started when complete
-     */
-    myBoards: function(callback) {
-        PDK.me('boards', { fields: Const.PIN_FIELDS }, callback);
-    }
+// Pinterest log in
+function logIn() {
+  PDK.login(function(response){
+    console.log(response);
+  })
+}
+
+// Determine auth state of the user
+function loggedIn() {
+  return !!PDK.getSession();
+}
+
+// Pinterest log out
+function logOut(){
+  PDK.logout();
+}
+
+// Return's the  authorized user’s profile, boards and pins
+function userInfo(){
+  PDK.me();
+}
+
+// Request a search for User's boards
+function myBoards(callback) {
+  PDK.me('boards', { fields: 'id,name,image[small]' }, callback);
+}
+
+// Return description and image of the pin
+var params = {
+  fields: 'note,image'
 };
 
-module.exports = Pinterest;
+// Retrieve pins on the board
+var pins = [];
+var pinCount = 0;
+function getPins(data, callback) {
+  PDK.request('/boards/Techies/pins', 'GET', function (response) { // TEMP BOARD : Techies
+    if (!response || response.error) {
+      alert('Error occurred');
+    } else {
+      pins = pins.concat(response.data);
+      console.log(pins); // TEST to see if pins are showing up
+      pinCount++;
+      if (response.hasNext) {
+        response.next(); // this will recursively go to this same callback
+      }
+    }
+  });
+}
+
+// Find duplicate pins
+function findDupPins(data) {
+  for(int i = 0; i < pinCount; i++){
+    for(int j = 0; j < pinCount; j++){
+      if(j != i) {
+        if(pin[i]==pin[j]){
+          deletePin(pin[i], function(response));
+        }
+      }
+    }
+  }
+}
+
+// Deleting a pin
+function deletePin(data, callback) {
+  PDK.request('/pins/', 'DELETE', data, callback);
+}

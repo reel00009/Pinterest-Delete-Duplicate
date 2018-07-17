8e62fe3afcb8a7319f6c37bfa700a89f6ae52959
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-17 16:15:33 -0700

    Function fix

--- pinterest.js
+++ pinterest.js
@@ -32,7 +32,7 @@ function findDupPins(data) {
 function pinterest() {
   // Function to log into user's Pinterest
   PDK.login({ scope : 'write_public, read_public' }, function(response) {
-    console.log(response);  // TEST 
+    console.log(response);  // TEST
 
     // Display the status of their login
     if (response.error || !response) {
@@ -40,19 +40,19 @@ function pinterest() {
     } else {
       document.getElementById('show').innerHTML = 'You are connected! We will now be deleting duplicate pins.';
       var user_id;
-      PDK.request('/v1/me/', 'GET', { fields: 'username' }, function(response) { // Get user information
+      PDK.me({ fields: 'username' }, function(response) { // Get user information
           user_id = response;
           console.log(response); // TEST
       });
-      user_id = 'tiananguyen99'; // TEMP USERNAME
+      user_id = 'tiananguyen99'; // TEST USERNAME
       // Ask user for which board user wants to search
       var board_id = prompt("Which board do you want to search?");
-      board_id='cooking-sessions'; // TEMP BOARD
+      board_id='cooking-sessions'; // TEST BOARD
       var pins = [];
       console.log("Test 3"); // TEST
       PDK.request('/boards/'+ user_id +'/'+ board_id +'/pins/', { fields: 'note,image[small]' }, function (response) {  // Get board information
           console.log(response); // TEST
-          console.log("Test 4"); // TEST
+          console.log("Test 5"); // TEST
           if (!response || response.error) {
             alert('Error occurred');
           } else {

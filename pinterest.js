878aa4cf0d2c6a7998fe04b132f4cd65ea7df8f3
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-23 11:00:16 -0700

    Delete duplicate fix

--- pinterest.js
+++ pinterest.js
@@ -56,8 +56,11 @@ function pinterest() {
           for (var i = 0; i < response.data.length; i++) {
             for (var j = 0; j < response.data.length; j++) {
               if ((j != i) && (response.data[j].note == response.data[i].note)) {
-                PDK.request('/v1/pins/' + response.data[i].id + '/', 'DELETE', response.data, function(response){});
+                for (var k = j; k < response.data.length; k++) {
+                  PDK.request('/v1/pins/' + response.data[i].id + '/', 'DELETE', response.data, function(response){});
+                }
               }
+              j--;
             }
           }
 
